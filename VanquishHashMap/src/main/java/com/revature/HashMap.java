2761e96d3cf1f2533bc614af1d6a18af93f4b4e6
Logan <logan.tan@wsu.edu>
2022-10-11 08:17:33 -0700

    Renamed Main and added javadoc comment to HashMap

---
+++ VanquishHashMap/src/main/java/com/revature/HashMap.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+public class HashMap {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
